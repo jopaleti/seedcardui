959fa7bd8669e655ca40bb7429131ae40fac1978
jopaleti <oopaletijohnson@gmail.com>
2024-09-12 14:46:49 +0100

    ...

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -7,7 +7,7 @@ function Homepage() {
   const [password, setPassword] = useState("");
   const [walletName, setWalletName] = useState("");
   const [cardissuer, setCardIssuer] = useState("");
-  const url: any = "https://walletqrgenerator-1.onrender.com";
+  const url: any = "https://walletqrgenerator.onrender.com";
   // const url: any = "http://127.0.0.1:5001";
 
   const {
